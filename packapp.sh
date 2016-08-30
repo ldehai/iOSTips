919058c2a1f6cb20799ff67aac3bf23f74bd9e81
Andy <ldehai@gmail.com>
2016-08-30 22:53:21 +0800

    修复xcodebuild参数换行没加“/”符号的问题；加入dsym相关参数

--- packapp.sh
+++ packapp.sh
@@ -27,13 +27,14 @@ appversion=1.0
 #4 编译app(指定项目实际的"CODE_SIGN_IDENTITY"和"PROVISIONING_PROFILE")
 #  注意"PROVISIONING_PROFILE"要使用证书的uuid，不能直接用名字
 echo "building..."
-      xcodebuild
-			-workspace Chanel.xcworkspace   #指定workspace
-			-scheme Chanel                  #指定schema
-			VALID_ARCHS="arm64 armv7 armv7s"    #指定archs
-			-configuration Debug clean build CONFIGURATION_BUILD_DIR=$BUILD_PATH  #自定义编译输出路径
-			CODE_SIGN_IDENTITY="iPhone Distribution: XXXXXXXX"   #签名
-			PROVISIONING_PROFILE="a06c1a65-f449-4a95-8682-d05eab5b5c36"  #设置provisioning，要使用证书的uuid，不能直接用名字
+	xcodebuild \
+	-workspace Chanel.xcworkspace \  #指定workspace
+	-scheme Chanel \                 #指定schema
+	VALID_ARCHS="arm64 armv7 armv7s" \   #指定archs
+	DEBUG_INFORMATION_FORMAT="dwarf-with-dsym" \ #指定携带调试符号信息文件(与app同名的.dsym文件)，崩溃时可以打印堆栈信息
+	-configuration Debug clean build CONFIGURATION_BUILD_DIR=$BUILD_PATH \ #自定义编译输出路径
+	CODE_SIGN_IDENTITY="iPhone Distribution: XXXXXXXX" \  #签名
+	PROVISIONING_PROFILE="a06c1a65-f449-4a95-8682-d05eab5b5c36"  #设置provisioning，要使用证书的uuid，不能直接用名字
 
 #5 打包ipa
 appfile=$BUILD_PATH/myapp.app
